499bc79dc92d2bc8fe3ac2c76be202a960153966
전상현 <sanghyun.jeon@architectkorea.com>
2022-01-12 14:31:14 +0900

    [KeyInputTest] 키입력 API를 이용해서 이벤트 방식으로 변경함

--- Test/KeyInputTest/main.cpp
+++ Test/KeyInputTest/main.cpp
@@ -1,27 +1,33 @@
-﻿#include <Windows.h>
-#include <stdio.h>
+﻿#include "stdafx.h"
+#include "KeyInput.h"
 
-void PrintOutBinary(unsigned char cByte)
+enum E_GAMEKEY_TYPE
 {
-    for (int i = 7; 0 <= i; i--)
-        printf("%c", (cByte >> i) & 0b00000001 ? '1' : '0');
-}
-
-void Query(int nVirtKey)
-{
-    unsigned short nState = GetKeyState(nVirtKey);
-    PrintOutBinary(nState >> 8);
-    printf(" ");
-    PrintOutBinary(nState & 0xFF);
-    printf("\n");
-}
+    GAMEKEY_LEFT,
+    GAMEKEY_RIGHT,
+    GAMEKEY_UP,
+    GAMEKEY_DOWN,
+    GAMEKEY_SELECT,
+};
 
 int main()
 {
+    CKeyInput input;
+    input.Register(GAMEKEY_LEFT,    'A');
+    input.Register(GAMEKEY_RIGHT,   'D');
+    input.Register(GAMEKEY_UP,      'W');
+    input.Register(GAMEKEY_DOWN,    'S');
+    input.Register(GAMEKEY_SELECT,  ' ');
+
     while (true)
     {
         Sleep(50);
-        Query('A');
+        
+        std::list<ST_KEYSTATE> listKeyStates;
+        input.Query(listKeyStates);
+
+        for (ST_KEYSTATE state : listKeyStates)
+            printf("key:`%c` pressed:%d\n", state.nVirtKey, state.bPressed);
     }
     return 0;
 }
