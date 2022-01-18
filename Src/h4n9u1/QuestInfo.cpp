a53724f614ade0732d7fe8907dcb88bfd8c4af3f
h4n9u1 <h4n9u1@gmail.com>
2022-01-18 16:00:10 +0900

    [h4n9u1][김훈민] 내 얼굴과 NPC 정보 추가함
    * 자기소개 작성 예정
    * 얼굴 대신 닉네임 아스키코드를 넣음

---
+++ Src/h4n9u1/QuestInfo.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "QuestInfo.h"
+
+static CQuestInfo g_QuestInfo;
+
+CQuestInfo::CQuestInfo()
+{
+	ExportAPIEntry(this);
+}
+
+CQuestInfo::~CQuestInfo()
+{
+	
+}
+
+void CQuestInfo::QueryNpc(std::vector<ST_NPC_INFO>& vecNPC)
+{
+	ST_NPC_INFO npc;
+	strcpy_s(npc.szName, 21, "취약점분석 김훈민");
+	npc.patch = 'p';
+	strcpy_s(npc.szGreetMessage, 61, "..."); // 자기소개 추가 예정
+
+	// 사진이 없으므로 닉네임의 아스키코드를 넣음
+	npc.vecBuffer.resize(6);
+	npc.vecBuffer[0] = " _   _    ___  _   _  _____  _   _  __  ";
+	npc.vecBuffer[1] = "| | | |  /   || \\ | ||  _  || | | |/  | ";
+	npc.vecBuffer[2] = "| |_| | / /| ||  \\| || |_| || | | |`| | ";
+	npc.vecBuffer[3] = "|  _  |/ /_| || . ` |\\____ || | | | | | ";
+	npc.vecBuffer[4] = "| | | |\\___  || |\\  |.___/ /| |_| |_| |_";
+	npc.vecBuffer[5] = "\\_| |_/    |_/\\_| \\_/\\____/  \\___/ \\___/";
+
+	vecNPC.push_back(npc);
+}
+
+void CQuestInfo::QueryQuest(std::vector<ST_QUEST_DATA*>& vecQuest)
+{
+	
+}
+
+
+
