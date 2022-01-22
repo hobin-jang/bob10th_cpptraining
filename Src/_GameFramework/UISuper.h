4a7863551285fc09f9da2bcf7b3290c730f94598
전상현 <sanghyun.jeon@seculetter.com>
2022-01-22 11:57:45 +0900

    [_GameFramework][전상현] UI의 생성자를 Create 함수로 분리

--- Src/_GameFramework/UISuper.h
+++ Src/_GameFramework/UISuper.h
@@ -2,9 +2,11 @@
 
 #include "Struct.h"
 
+class CDlgSuper;
 class CUISuper
 {
 protected:
+	CDlgSuper* m_pParent;
 	int m_nLeft;
 	int m_nTop;
 	int m_nRight;
@@ -12,11 +14,12 @@ protected:
 
 	std::string m_strText;
 
-	CUISuper(ST_VECTOR ptLeftTop, ST_VECTOR ptRightBtm);
-
-	void SetText(std::string strText) { m_strText = strText; }
+	CUISuper(void);
 
 public:
+	virtual void Create(CDlgSuper* pParent, ST_VECTOR ptLeftTop, ST_VECTOR ptRightBtm);
+	virtual void SetText(std::string strText) { m_strText = strText; }
+
 	virtual void OnDraw(std::vector<std::string>& vecBuffer);
 };
 
