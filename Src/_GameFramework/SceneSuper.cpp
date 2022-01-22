2a84cf28375a64e501ed0e7cb9010fc55656cd3a
전상현 <sanghyun.jeon@seculetter.com>
2022-01-22 11:55:45 +0900

    [_GameFramework][전상현] 다이얼로그 UI 생성(Scene의 DoModal 함수를 DlgSuper로 이동)

--- Src/_GameFramework/SceneSuper.cpp
+++ Src/_GameFramework/SceneSuper.cpp
@@ -1,22 +1,13 @@
 #include "pch.h"
 #include "SceneSuper.h"
-#include "KeyInput.h"
-#include "Camera.h"
-#include "ConsoleOutput.h"
-
-CKeyInput CSceneSuper::m_Input;
 
 CSceneSuper::CSceneSuper(void)
-	: m_bIsEndded(false)
-	, m_nExitCode(0)
+	: CDlgSuper(nullptr)
 {
 }
 
 CSceneSuper::~CSceneSuper(void)
 {
-    for (CUISuper* pUI : m_listUI)
-        delete pUI;
-    m_listUI.clear();
 }
 
 void CSceneSuper::OnCreate(void)
@@ -30,67 +21,4 @@ void CSceneSuper::OnCreate(void)
     m_Input.Register(GAMEKEY_ESC, VK_ESCAPE);
 
     m_Output.SetViewPort(g_nConsoleW + 1, g_nConsoleH + 1);
-}
-
-void CSceneSuper::Close(int nExitCode)
-{
-	m_bIsEndded = true;
-	m_nExitCode = nExitCode;
-}
-
-void CSceneSuper::AddUI(CUISuper* pNewUI)
-{
-    m_listUI.push_back(pNewUI);
-}
-
-int CSceneSuper::DoModal(void)
-{
-    OnCreate();
-
-    static DWORD dwLastUpdateTick = GetTickCount();
-    while (!m_bIsEndded)
-    {
-        std::list<ST_KEYSTATE> listKeyState;
-        m_Input.Query(listKeyState);
-        OnInput(listKeyState);
-
-        OnUpdate(g_dDeltaTime);
-        m_Camera.Update(g_dDeltaTime);
-
-        CDisplayBuffer vecBackBuffer = m_Output.GetBackBuffer();
-        OnDrawWorld(vecBackBuffer);
-
-        CDisplayBuffer vecDisplayBuffer;
-        m_Output.Flip(m_Camera.GetViewPos(), vecDisplayBuffer);
-        OnDrawUI(vecDisplayBuffer);
-
-        m_Output.Render(vecDisplayBuffer);
-
-        const DWORD dwCurrentTick = GetTickCount();
-        if (dwCurrentTick < dwLastUpdateTick)
-            Sleep(dwLastUpdateTick - dwCurrentTick);
-        dwLastUpdateTick += g_nDeltaTick;
-    }
-
-	return m_nExitCode;
-}
-
-void CSceneSuper::OnInput(std::list<ST_KEYSTATE>& listKeyState)
-{
-}
-
-void CSceneSuper::OnUpdate(double dElapsedTime)
-{
-}
-
-void CSceneSuper::OnDrawWorld(std::vector<std::string>& vecBuffer)
-{
-}
-
-void CSceneSuper::OnDrawUI(std::vector<std::string>& vecBuffer)
-{
-	for (CUISuper* pUI : m_listUI)
-	{
-		pUI->OnDraw(vecBuffer);
-	}
 }
--- Src/_GameFramework/SceneSuper.h
+++ Src/_GameFramework/SceneSuper.h
@@ -4,35 +4,14 @@
 #include <vector>
 #include <string>
 #include "Struct.h"
-#include "UISuper.h"
-#include "KeyInput.h"
-#include "Camera.h"
-#include "ConsoleOutput.h"
+#include "DlgSuper.h"
 
-class CSceneSuper
+class CSceneSuper : public CDlgSuper
 {
-	static CKeyInput m_Input;
-	CCamera m_Camera;
-	CConsoleOutput m_Output;
-
-	std::list<CUISuper*> m_listUI;
-	bool m_bIsEndded;
-	int m_nExitCode;
-
 protected:
 	CSceneSuper(void);
 	virtual ~CSceneSuper(void);
 
-	virtual void Close(int nExitCode);
-	virtual void AddUI(CUISuper* pNewUI);
-
 	virtual void OnCreate(void);
-	virtual void OnInput(std::list<ST_KEYSTATE>& listKeyState);
-	virtual void OnUpdate(double dElapsedTime);
-	virtual void OnDrawWorld(std::vector<std::string>& vecBuffer);
-	virtual void OnDrawUI(std::vector<std::string>& vecBuffer);
-
-public:
-	virtual int DoModal(void);
 };
 
