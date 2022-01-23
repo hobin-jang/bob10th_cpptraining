66b4d82a59017b506c0aa760bec1bcba9009a2b6
전상현 <sanghyun.jeon@seculetter.com>
2022-01-23 10:02:51 +0900

    [_GameFramework][전상현] 게임개발 저장소로 이동

--- Src/_GameFramework/DrawFunc.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Struct.h"
-
-void Clear(CDisplayBuffer& buffer);
-void DrawRectangle(CDisplayBuffer& buffer, ST_VECTOR ptLeftTop, ST_VECTOR ptRightBtm);
-void DrawRectangle(CDisplayBuffer& buffer, int nLeft, int nTop, int nRight, int nBottom);
