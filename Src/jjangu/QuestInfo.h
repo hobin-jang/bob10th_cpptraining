e6e7fd5c7d68dba603a218f33e395ea49791c5a9
jjanguu <tk3414@naver.com>
2022-01-24 17:16:51 +0900

    [jjanguu][김동현] NPC 프로필 추가 작성

--- Src/jjangu/QuestInfo.h
+++ Src/jjangu/QuestInfo.h
@@ -11,6 +11,11 @@ public:
 	~CQuestInfo(void);
 
 	void QueryNpc(std::vector<ST_NPC_INFO>& vecNPC);
+	void QueryNpcEx(std::vector<ST_QUEST_NPC_DATA>& vecNPC);
 	void QueryQuest(std::vector<ST_QUEST_DATA*>& vecQuest);
+};
+
+class QuestInfo
+{
 };
 
