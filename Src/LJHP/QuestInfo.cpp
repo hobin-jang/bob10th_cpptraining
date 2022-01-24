93e5387f52ae87670d70fd8bca1a7ffd99a28bb1
LJHP-CODER <leejuh1621@gmail.com>
2022-01-24 16:47:48 +0900

    [LJHP][이주협] 게임용 퀘스트 dll 추가

--- Src/LJHP/QuestInfo.cpp
+++ Src/LJHP/QuestInfo.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "QuestInfo.h"
 
+static CQuestInfo g_QuestInfo;
+
 CQuestInfo::CQuestInfo(void) {
 	ExportAPIEntry(this);
 }
@@ -13,7 +15,7 @@ void CQuestInfo::QueryNpc(std::vector<ST_NPC_INFO>& vecNPC)
 {
 	ST_NPC_INFO npc;
 	strcpy_s(npc.szName, 10, "이주협");
-	npc.patch = 'p';
+	npc.patch = 'X';
 	strcpy_s(npc.szGreetMessage, 61, "안녕하세요.");
 
 	npc.vecBuffer.resize(60);
@@ -77,6 +79,48 @@ void CQuestInfo::QueryNpc(std::vector<ST_NPC_INFO>& vecNPC)
 	npc.vecBuffer[57] = "$$$$$$$$$$$#$$$$$$$$~.**=#$!$###$$$$##$$$$########";
 	npc.vecBuffer[58] = "$$$$$$$=$$$$$$$$$$$$@-===$#$$##$$$$#@#$$$$#$######";
 	npc.vecBuffer[59] = "$$$$$$$$$$$$$$#$$$$$@#===###$##$$$$##$$$$#########";
+	vecNPC.push_back(npc);
+}
+
+void CQuestInfo::QueryNpcEx(std::vector<ST_QUEST_NPC_DATA>& vecNPC ) {
+	ST_QUEST_NPC_DATA npc;
+	npc.Pos.x = 11;
+	npc.Pos.y = 5;
+	npc.Pos.z = 25;
+	npc.nNpcID = 151;
+	npc.strTrack = "취약점분석트랙";
+	npc.strName = "이주협";
+	npc.nAge = 18;
+	npc.strMBTI = "INFJ";
+	npc.cPatch = 'X';
+	npc.strMessage = "안녕하세요 이주협입니다. 해킹은 재밌다!";
+
+	npc.vecBuffer.resize(25);
+	npc.vecBuffer[0] = "           .@+           ";
+	npc.vecBuffer[1] = "         +@###@%         ";
+	npc.vecBuffer[2] = "        @###%###@*       ";
+	npc.vecBuffer[3] = "       @####%####@:      ";
+	npc.vecBuffer[4] = "      @###########@      ";
+	npc.vecBuffer[5] = "      %############=     ";
+	npc.vecBuffer[6] = "     @#####@@@#####@     ";
+	npc.vecBuffer[7] = "     #####@@@@@######    ";
+	npc.vecBuffer[8] = "    @###@       ####@    ";
+	npc.vecBuffer[9] = "    ##@ @+     :@ @##%   ";
+	npc.vecBuffer[10] = "   @#@#+ -@   %* .=@@#   ";
+	npc.vecBuffer[11] = "  .%@@-   +# -@    @@#%  ";
+	npc.vecBuffer[12] = "  @#@@  :       :  @@#%  ";
+	npc.vecBuffer[13] = "  @%@@ @@@ : * @@@ @@@#+ ";
+	npc.vecBuffer[14] = " @#@@@  @@:: *.@@  @@@#@ ";
+	npc.vecBuffer[15] = " @#@@@     - =     @@@## ";
+	npc.vecBuffer[16] = " %#@@@  *       .  @@@##:";
+	npc.vecBuffer[17] = " @#@@@% . .#    @ #@@@## ";
+	npc.vecBuffer[18] = " @##@@@ @       @.@@@##@ ";
+	npc.vecBuffer[19] = "  @#@@@ @ #* .* @.@@@#@  ";
+	npc.vecBuffer[20] = "   @#@@ @@@@%@@@@ @@##   ";
+	npc.vecBuffer[21] = "    @#@   @:.:@   @@#.   ";
+	npc.vecBuffer[22] = "     @%@         @@#.    ";
+	npc.vecBuffer[23] = "      @@@       @@#      ";
+	npc.vecBuffer[24] = "       @%@@@@@@@@#       ";
 	vecNPC.push_back(npc);
 }
 
