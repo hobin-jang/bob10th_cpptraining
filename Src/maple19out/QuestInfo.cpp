78c50be8bacce1cfa2e6d5f4de144e4f680c0937
maple19out <maple19out@naver.com>
2022-01-24 14:59:49 +0900

    [maple19out][강민석] 추가 프로필 작성을 위한 프레임워크 변경

--- Src/maple19out/QuestInfo.cpp
+++ Src/maple19out/QuestInfo.cpp
@@ -15,7 +15,12 @@ CQuestInfo::~CQuestInfo(void)
 void CQuestInfo::QueryNpc(std::vector<ST_NPC_INFO>& vecNPC)
 {
     ST_NPC_INFO npc;
+    npc.id = 134;
     strcpy_s(npc.szName, 21, "강민석 멘티");
+    npc.x = 10;
+    npc.y = 10;
+    npc.w = 0.5;
+    npc.h = 0.5;
     npc.patch = 'm';
     strcpy_s(npc.szGreetMessage, 61, "오늘도 삽질 화이팅!");
 
@@ -64,6 +69,51 @@ void CQuestInfo::QueryNpc(std::vector<ST_NPC_INFO>& vecNPC)
     vecNPC.push_back(npc);
 }
 
+void CQuestInfo::QueryNpcEx(std::vector<ST_QUEST_NPC_DATA>& vecNPC)
+{
+    ST_QUEST_NPC_DATA npc;
+    npc.Pos.x = 10;
+    npc.Pos.y = 10;
+    npc.Pos.z = 1;
+    npc.nNpcID = 134;
+    npc.strTrack = "취약점분석트랙";
+    npc.strName = "강민석";
+    npc.nAge = 26;
+    npc.strMBTI = "ISFJ이고 A형입니다";
+    npc.cPatch = 'M';
+    npc.strMessage = "언제 끝날까 BoB?";
+    npc.vecBuffer.resize(25);
+    
+    npc.vecBuffer[0] = "                     .,,...,.                     ";
+    npc.vecBuffer[1] = "                 ,;;!!*!!!!!!:.                   ";
+    npc.vecBuffer[2] = "               ,;!****====*=***-.                 ";
+    npc.vecBuffer[3] = "              -;****===========*;,                ";
+    npc.vecBuffer[4] = "             .!!**===============*-               ";
+    npc.vecBuffer[5] = "             !!!**================*-              ";
+    npc.vecBuffer[6] = "            -!!**==================*.             ";
+    npc.vecBuffer[7] = "           .;***=*==================,             ";
+    npc.vecBuffer[8] = "           -!****===================!             ";
+    npc.vecBuffer[9] = "           :****=*==================*             ";
+    npc.vecBuffer[10] = "           !******==================*-            ";
+    npc.vecBuffer[11] = "           *******===================:            ";
+    npc.vecBuffer[12] = "          ,****=***==================:            ";
+    npc.vecBuffer[13] = "          :****=****=================~            ";
+    npc.vecBuffer[14] = "          ;!***==****========*=======-            ";
+    npc.vecBuffer[15] = "          ,***===*!**=====****=======.            ";
+    npc.vecBuffer[16] = "           ***==*:;;;!;-,~;:;!======*             ";
+    npc.vecBuffer[17] = "           ;*;!;-~-~-.   ..,~:;;!*==:             ";
+    npc.vecBuffer[18] = "           ,:-..~. .,..  .,~,,,;~:!*.             ";
+    npc.vecBuffer[19] = "           ..~,:...-,,, .,-,----~-!;.             ";
+    npc.vecBuffer[20] = "            .- ~.,.:~.-..~,-;~~--~!~              ";
+    npc.vecBuffer[21] = "             . ;  ..,.- .~,,,,,,,,:,              ";
+    npc.vecBuffer[22] = "                .  .. .  ~......-,~.              ";
+    npc.vecBuffer[23] = "                ,,       ,,. ..-,,,.              ";
+    npc.vecBuffer[24] = "                         .,,. ...,,.              ";
+
+    vecNPC.push_back(npc);
+}
+
 void CQuestInfo::QueryQuest(std::vector<ST_QUEST_DATA*>& vecQuest)
 {
+
 }
