d5e59c88f064a054c653c534c05034499dce64bd
LINKER <temp2temp@daum.net>
2022-01-28 16:23:41 +0900

    LINKER 불필요한 코드 삭제

--- Src/LINKER/QuestInfo.cpp
+++ Src/LINKER/QuestInfo.cpp
@@ -22,73 +22,62 @@ void CQuestInfo::QueryNpcEx(std::vector<ST_QUEST_NPC_DATA>& vecNPC)
 	npc.strTrack = "보안제품개발트랙";
 	npc.strName = "이찬우(LINKER)";
 	npc.nAge = 24;
-	npc.strMBTI = "ENFJ-T이고 A형 입니다.";
-	npc.cPatch = 'L';
+	npc.strMBTI = "ENFJ-T, A형 입니다.";
+	npc.cPatch = 'C';
 	npc.strMessage = "어이 너 나랑 코딩하지 않을래?";
 	
-	npc.vecBigImage.resize(62);
-	npc.vecBigImage[0] = "                                                       ";
-	npc.vecBigImage[1] = "                   ............                       ";
-	npc.vecBigImage[2] = "                 .,~#=~,,*##@#!:,                     ";
-	npc.vecBigImage[3] = "               .,=##@@@#$@#@@@##=-.                   ";
-	npc.vecBigImage[4] = "              .;$##@@@@@@@@@@@@##$!.                  ";
-	npc.vecBigImage[5] = "              :$$##@@@@@@@@@@@###$$:                  ";
-	npc.vecBigImage[6] = "             ,!=$#@@@@@@@@@@@@@##$#*,                 ";
-	npc.vecBigImage[7] = "            .;$$#@@@@@@@@@@@@@@#####:                 ";
-	npc.vecBigImage[8] = "           .:=$#@@@@@@@@@@@####@##$#*,                ";
-	npc.vecBigImage[9] = "           -=$$@@@@@@@@@@@@###$@#$$$$!.               ";
-	npc.vecBigImage[10] = "           ~###@@@@@@@#@#@###$#$#$$$$$~               ";
-	npc.vecBigImage[11] = "           :####@@@#@##$$###$$#$$$$$##:               ";
-	npc.vecBigImage[12] = "           ;#$$########$#$$$$$$$$$$###:               ";
-	npc.vecBigImage[13] = "          -=$$##$$$##$$$$$$$$$$$$$$$##:               ";
-	npc.vecBigImage[14] = "          ~$$$$$$$$$$$$$=#$$$$$$$$####:               ";
-	npc.vecBigImage[15] = "          -=$$$$$$$$$$$$#####$####@###~               ";
-	npc.vecBigImage[16] = "          .;#$$##$#####@####@###@@@##$~               ";
-	npc.vecBigImage[17] = "           :##$#######@@####@@##@@@@#:                ";
-	npc.vecBigImage[18] = "           :#$$##########@##@#@@@@@##:                ";
-	npc.vecBigImage[19] = "            :##$$#$$*!:-.,-;$#$#@@###:                ";
-	npc.vecBigImage[20] = "            -==;:~,.,,.   .,;--:$$=#*,                ";
-	npc.vecBigImage[21] = "             :;,... ..,    ....,,-:;..                ";
-	npc.vecBigImage[22] = "             .--...,~...  ,,**~..,;~.                 ";
-	npc.vecBigImage[23] = "             ~~,.,,~!-.   .-......,,                  ";
-	npc.vecBigImage[24] = "             ,-...    ,.  ~     .,,,                  ";
-	npc.vecBigImage[25] = "             .,.  ,   ,    ,  .  .,,                  ";
-	npc.vecBigImage[26] = "             ...   , ,      , ,  .,,                  ";
-	npc.vecBigImage[27] = "             .,.                 .,,                  ";
-	npc.vecBigImage[28] = "             .,.                 .,.                  ";
-	npc.vecBigImage[29] = "              ,,.       ..      ...                   ";
-	npc.vecBigImage[30] = "               ..      ,...     .,.                   ";
-	npc.vecBigImage[31] = "               ..               .,,                   ";
-	npc.vecBigImage[32] = "               .,.              ...                   ";
-	npc.vecBigImage[33] = "                ..   ,-,,,,-    ..                    ";
-	npc.vecBigImage[34] = "                ..   .......   .,,                    ";
-	npc.vecBigImage[35] = "                 ..     ..     .,.                    ";
-	npc.vecBigImage[36] = "                 ...          .,,                     ";
-	npc.vecBigImage[37] = "                 ,,..        .,-,                     ";
-	npc.vecBigImage[38] = "                 ,-,..     ..,--,.                    ";
-	npc.vecBigImage[39] = "                 .,--,.....,,,,.,,                    ";
-	npc.vecBigImage[40] = "                 .,,-,,,,,,,,,,.,,                    ";
-	npc.vecBigImage[41] = "                 .,,,-,,,,,,,,....                    ";
-	npc.vecBigImage[42] = "                --...,,,,,,,.....-;,                  ";
-	npc.vecBigImage[43] = "               --,....,.,.........-:~,                ";
-	npc.vecBigImage[44] = "             ,~;-............. ...,:~~:,              ";
-	npc.vecBigImage[45] = "         ..~~~~,......... . . ... .-::~::,..          ";
-	npc.vecBigImage[46] = "       .,:::~~~,. ..  ...     ..   ,::~:::::,.        ";
-	npc.vecBigImage[47] = "     .,::~::~~~.                   ,::~~::~::~..      ";
-	npc.vecBigImage[48] = "   .,:~:::::::~.                   .~~~~:::::::~..    ";
-	npc.vecBigImage[49] = "  ,~~:~::::~~~~.                   .~~~~:::~~:::~:    ";
-	npc.vecBigImage[50] = ",,~::::::~:~~~~.                   ,~~~~::~::~::::    ";
-	npc.vecBigImage[51] = "::~::~~~~~:~~~~,      LINKER      .-~~~::~~:::~:~:    ";
-	npc.vecBigImage[52] = "~:~:~~~~~::~~~~-.                 .-~~~~~:~~:~~:~:    ";
-	npc.vecBigImage[53] = "~:~~~:~:~~~~~~~-.                 .~~~~~:~~~::~~::    ";
-	npc.vecBigImage[54] = "~:~:-:~:~~~~~~--.                 .~~~~~:~~~~:-~:~    ";
-	npc.vecBigImage[55] = "~:~:~~~~~~~~~~~~.                 .~~~~~:~~~~~-~~~    ";
-	npc.vecBigImage[56] = "~~-~~~~~~~:~-~~~.                 .~~~~~~~~~~:~~~~    ";
-	npc.vecBigImage[57] = "~~~~~:~~~~~~~~~~~.                .~~~~~~~~--~~:~:    ";
-	npc.vecBigImage[58] = "-:~:~:~:~~~~~~~~~.                ,~~~~:~~:~-~::~:    ";
-	npc.vecBigImage[59] = "-~~:~~~~~~~~-~~:-.               .~~~~--~~~~~~~:~:    ";
-	npc.vecBigImage[60] = "~~~~-~~~~~~~~~~~-.               .-~~~~~~~~~~~~:~!    ";
-	npc.vecBigImage[61] = "~~~--~::~~~:~~~~~.               .-~~~:~~~~~~~~::*    ";
+	npc.vecBigImage.resize(50);
+	npc.vecBigImage[0 ] = "                 .,~#=~,,*##@#!:,                     ";
+	npc.vecBigImage[1 ] = "               .,=##@@@#$@#@@@##=-.                   ";
+	npc.vecBigImage[2 ] = "              .;$##@@@@@@@@@@@@##$!.                  ";
+	npc.vecBigImage[3 ] = "              :$$##@@@@@@@@@@@###$$:                  ";
+	npc.vecBigImage[4 ] = "             ,!=$#@@@@@@@@@@@@@##$#*,                 ";
+	npc.vecBigImage[5 ] = "            .;$$#@@@@@@@@@@@@@@#####:                 ";
+	npc.vecBigImage[6 ] = "           .:=$#@@@@@@@@@@@####@##$#*,                ";
+	npc.vecBigImage[7 ] = "           -=$$@@@@@@@@@@@@###$@#$$$$!.               ";
+	npc.vecBigImage[8 ] = "           ~###@@@@@@@#@#@###$#$#$$$$$~               ";
+	npc.vecBigImage[9 ] = "           :####@@@#@##$$###$$#$$$$$##:               ";
+	npc.vecBigImage[10] = "           ;#$$########$#$$$$$$$$$$###:               ";
+	npc.vecBigImage[11] = "          -=$$##$$$##$$$$$$$$$$$$$$$##:               ";
+	npc.vecBigImage[12] = "          ~$$$$$$$$$$$$$=#$$$$$$$$####:               ";
+	npc.vecBigImage[13] = "          -=$$$$$$$$$$$$#####$####@###~               ";
+	npc.vecBigImage[14] = "          .;#$$##$#####@####@###@@@##$~               ";
+	npc.vecBigImage[15] = "           :##$#######@@####@@##@@@@#:                ";
+	npc.vecBigImage[16] = "           :#$$##########@##@#@@@@@##:                ";
+	npc.vecBigImage[17] = "            :##$$#$$*!:-.,-;$#$#@@###:                ";
+	npc.vecBigImage[18] = "            -==;:~,.,,.   .,;--:$$=#*,                ";
+	npc.vecBigImage[19] = "             :;,... ..,    ....,,-:;..                ";
+	npc.vecBigImage[20] = "             .--...,~...  ,,**~..,;~.                 ";
+	npc.vecBigImage[21] = "             ~~,.,,~!-.   .-......,,                  ";
+	npc.vecBigImage[22] = "             ,-...    ,.  ~     .,,,                  ";
+	npc.vecBigImage[23] = "             .,.  ,   ,    ,  .  .,,                  ";
+	npc.vecBigImage[24] = "             ...   , ,      , ,  .,,                  ";
+	npc.vecBigImage[25] = "             .,.                 .,,                  ";
+	npc.vecBigImage[26] = "             .,.                 .,.                  ";
+	npc.vecBigImage[27] = "              ,,.       ..      ...                   ";
+	npc.vecBigImage[28] = "               ..      ,...     .,.                   ";
+	npc.vecBigImage[29] = "               ..               .,,                   ";
+	npc.vecBigImage[30] = "               .,.              ...                   ";
+	npc.vecBigImage[31] = "                ..   ,-,,,,-    ..                    ";
+	npc.vecBigImage[32] = "                ..   .......   .,,                    ";
+	npc.vecBigImage[33] = "                 ..     ..     .,.                    ";
+	npc.vecBigImage[34] = "                 ...          .,,                     ";
+	npc.vecBigImage[35] = "                 ,,..        .,-,                     ";
+	npc.vecBigImage[36] = "                 ,-,..     ..,--,.                    ";
+	npc.vecBigImage[37] = "                 .,--,.....,,,,.,,                    ";
+	npc.vecBigImage[38] = "                 .,,-,,,,,,,,,,.,,                    ";
+	npc.vecBigImage[39] = "                 .,,,-,,,,,,,,....                    ";
+	npc.vecBigImage[40] = "                --...,,,,,,,.....-;,                  ";
+	npc.vecBigImage[41] = "               --,....,.,.........-:~,                ";
+	npc.vecBigImage[42] = "             ,~;-............. ...,:~~:,              ";
+	npc.vecBigImage[43] = "         ..~~~~,......... . . ... .-::~::,..          ";
+	npc.vecBigImage[44] = "       .,:::~~~,. ..  ...     ..   ,::~:::::,.        ";
+	npc.vecBigImage[45] = "     .,::~::~~~.                   ,::~~::~::~..      ";
+	npc.vecBigImage[46] = "   .,:~:::::::~.                   .~~~~:::::::~..    ";
+	npc.vecBigImage[47] = "  ,~~:~::::~~~~.     LINKER        .~~~~:::~~:::~:    ";
+	npc.vecBigImage[48] = ",,~::::::~:~~~~.                   ,~~~~::~::~::::    ";
+	npc.vecBigImage[49] = "::~::~~~~~:~~~~,                  .-~~~::~~:::~:~:    ";
+
 
 	npc.vecSmallImage.resize(25);
         
@@ -130,8 +119,8 @@ void CQuestInfo::QueryNpcEx(std::vector<ST_QUEST_NPC_DATA>& vecNPC)
 		"어... 지옥길에 온걸 환영합니다.\n"
 		;
 
-	npc.strContents4 = "ㅎ2ㅎ2ㅎ2 반갑습니다 ~ \n"
-		"어... 지옥길에 온걸 환영합니다.\n"
+	npc.strContents4 = "오호... 나의 부탁을 들어줘서 감사합니다. \n"
+		"엔딩 메시지 구상중...\n"
 		;
 
 	vecNPC.push_back(npc);
