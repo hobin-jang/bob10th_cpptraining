5f1ec7e49a9b4589bbec01b1fcc42c154ae0b552
mochang2 <qjsrodksro@gmail.com>
2022-02-11 11:01:19 +0900

    [lcm][이창모] minigame 오류 해결

--- Src/lcm/MiniGame.cpp
+++ Src/lcm/MiniGame.cpp
@@ -10,11 +10,6 @@ enum E_GAMEKEY_TYPE
 CMiniGame::CMiniGame(void)
 	: CDlgSuper()
 {
-	g_Output.InitConsole("미니게임 테스트", g_nConsoleW * 2 + 3, g_nConsoleH + 3);
-	g_Output.SetViewPort(g_nConsoleW, g_nConsoleH);
-
-	g_Input.Register(VK_SPACE, GAMEKEY_SELECT);
-	g_Input.Register(VK_ESCAPE, GAMEKEY_ESCAPE);
 }
 
 CMiniGame::~CMiniGame(void)
@@ -23,11 +18,10 @@ CMiniGame::~CMiniGame(void)
 
 void CMiniGame::OnCreate(void)
 {
-	ST_SIZE stWindowSize = { 30, 30 };
+	ST_SIZE stWindowSize = { g_nConsoleW, g_nConsoleH };
 	ST_POINT stCenterPos = { (g_nConsoleW - stWindowSize.cx) / 2,  (g_nConsoleH - stWindowSize.cy) / 2 };
 	SetPos(stCenterPos);
 	SetSize(stWindowSize);
-
 
 	m_TextUI.Create(this, ST_POINT{ 0, 0 }, ST_SIZE{ 20, 3 });
 	m_TextUI.SetText("Hello world!!");
@@ -39,13 +33,22 @@ void CMiniGame::OnClose(void)
 
 void CMiniGame::OnInput(std::list<ST_KEYSTATE>& listKeyState)
 {
-	__super::OnInput(listKeyState);
 	for (ST_KEYSTATE key : listKeyState)
 	{
 		if (key.bPressed && key.nID == GAMEKEY_SELECT)
 			Close(0); // 0으로 끝내면 성공
-		if (key.bPressed && key.nID == GAMEKEY_ESCAPE)
+
+		if (key.bPressed && key.nID == GAMEKEY_ESC)
 			Close(-1);	// 0 이외의 값으로 끝내면 실패
+
+		if (key.bPressed && key.nID == GAMEKEY_MENU)
+		{
+			std::string strMsg;
+			if (0 == CEditBox::Show(this, "텍스트를 입력하세요", strMsg))
+				m_TextUI.SetText(strMsg);
+
+			//int nAnswer = CMessageBox::Show(this, "메시지 박스 테스트", MB_YESNO);
+		}
 	}
 }
 
