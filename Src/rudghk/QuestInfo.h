991a3f49a5e9411f86af96d2854eb66534972cae
rudghk <7942jgh@gmail.com>
2022-02-14 15:11:25 +0900

    [rudghk][정경화] 퀘스트 수정

--- Src/rudghk/QuestInfo.h
+++ Src/rudghk/QuestInfo.h
@@ -10,5 +10,7 @@ public:
 
     void QueryNpcEx(std::vector<ST_QUEST_NPC_DATA>& vecNPC);
     void QueryQuestObject(std::vector<ST_QUEST_OBJECT>& vecQuestObject);
+    void QueryQuestItem(std::vector< ST_QUEST_ITEM_DATA>& vecQuestItem);
+    void QueryQuestMonster(std::vector< ST_QUEST_MONSTER_DATA>& vecQuestMonster);
     void QueryQuest(std::vector<ST_QUEST_DATA>& vecQuest);
 };
