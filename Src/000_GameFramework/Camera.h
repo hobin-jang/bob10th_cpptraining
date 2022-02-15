f31fff6ed2ffc6bec56ef8907e268b647f921b25
전상현 <profrog.jeon@gmail.com>
2022-02-15 19:12:22 +0900

    [000_GameFramework][전상현] 카메라 오프셋을 스택 방식으로 변경

--- Src/000_GameFramework/Camera.h
+++ Src/000_GameFramework/Camera.h
@@ -1,18 +1,22 @@
 #pragma once
 
 #include "Struct.h"
+#include <stack>
 
 class CCamera
 {
 	ST_VECTOR m_TargetPos;
 	ST_VECTOR m_Pos;
-	ST_POINT m_ptCenterOffset;
+
+	std::stack<ST_POINT> m_OffsetStack;
 
 public:
 	CCamera(void);
 	~CCamera(void);
 
 	void CenterOffset(ST_POINT ptCenterOffset);
+	void PushCenterOffset(ST_POINT ptCenterOffset);
+	void PopCenterOffset(void);
 	void SetViewPos(int x, int y, bool bForcely = false);
 	void SetViewPos(ST_POINT pos, bool bForcely = false);
 	void SetViewPos(ST_POINT3 pos, bool bForcely = false);
